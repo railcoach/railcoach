c01c1730ce0c3a082706ddee4ee27fbdbbe723e4
Timonv <timonv@gmail.com>
2010-10-02 16:56:01 +0200

    Set rails to log deprecation warnings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,10 @@ Dynamic::Application.configure do
   # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = false
 
+
+  # Make sure deprecation warnings are logged
+  config.active_support.deprecation = :log
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
