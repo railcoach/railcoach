787568863e890c336d7017011496930c21837dc0
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-10-04 23:12:16 +0200

    add a title to user (e.g. the text under the name on the static page)
    href #9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100922101833) do
+ActiveRecord::Schema.define(:version => 20101004210219) do
 
   create_table "project_memberships", :force => true do |t|
     t.integer  "user_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20100922101833) do
     t.string   "email"
     t.string   "password_salt"
     t.string   "persistence_token"
+    t.string   "title"
   end
 
 end
