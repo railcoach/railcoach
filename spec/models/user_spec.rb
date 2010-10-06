2a9edf34911fb40402a1e4734ac09810c29dbd65
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-10-06 10:54:41 +0200

    move test User model rspec unit tests to spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe User do
+describe User, "when first created" do
   before(:each) do
     @user = User.new
   end
@@ -8,6 +8,18 @@ describe User do
   it "should not save example" do
     @user.should_not be_valid
   end
-
 end
 
+describe User, "that is already created" do
+  before(:each) do
+    @user = users(:edsgerd)
+  end
+
+  it "should have a name" do
+    user.name.should == 'Edsger Dijkstra'
+  end
+
+  it "should have a title" do
+    user.title.should == 'Genius'
+  end
+end
