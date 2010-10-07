071109f485e5b3ab7c55d4e1e77221a699c72634
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-10-07 22:30:33 +0200

    refactored RSpec tests for user to use contexts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,33 +1,35 @@
 require 'spec_helper'
 
-describe User, "when first created" do
-  before(:each) do
-    @user = User.new
-  end
+describe User do
+  context "when first created" do
+    before(:each) do
+      @user = User.new
+    end
 
-  it "should not save example" do
-    @user.should_not be_valid
-  end
-end
-
-describe User, "that is already created" do
-  before(:each) do
-    @user = User.new do |u|
-      u.first_name = 'Edsger'
-      u.last_name = 'Dijkstra'
-      u.title = 'Genius'
+    it "should not save example" do
+      @user.should_not be_valid
     end
   end
 
-  it "should have a name" do
-    @user.name.should == 'Edsger Dijkstra'
-  end
+  context "that is already created" do
+    before(:each) do
+      @user = User.new do |u|
+        u.first_name = 'Edsger'
+        u.last_name = 'Dijkstra'
+        u.title = 'Genius'
+      end
+    end
 
-  it "should have a title" do
-    @user.title.should == 'Genius'
-  end
+    it "should have a name" do
+      @user.name.should == 'Edsger Dijkstra'
+    end
 
-  it "should be male" do
-    @user.gender.should == 'm'
+    it "should have a title" do
+      @user.title.should == 'Genius'
+    end
+
+    it "should be male" do
+      @user.gender.should == 'm'
+    end
   end
 end
