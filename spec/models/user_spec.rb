28a60773560acd7c28a5f98e962dec013649c8e2
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-10-07 20:35:10 +0200

    fixed rspec model tests
    Refs #8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,18 +12,22 @@ end
 
 describe User, "that is already created" do
   before(:each) do
-    @user = users(:edsgerd)
+    @user = User.new do |u|
+      u.first_name = 'Edsger'
+      u.last_name = 'Dijkstra'
+      u.title = 'Genius'
+    end
   end
 
   it "should have a name" do
-    user.name.should == 'Edsger Dijkstra'
+    @user.name.should == 'Edsger Dijkstra'
   end
 
   it "should have a title" do
-    user.title.should == 'Genius'
+    @user.title.should == 'Genius'
   end
 
   it "should be male" do
-    user.gender.should == 'm'
+    @user.gender.should == 'm'
   end
 end
