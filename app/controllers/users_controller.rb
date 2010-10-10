8861a38929a261522d4f515b4173248ef7f89825
jasperf <jasper.froeling@gmail.com>
2010-10-10 13:36:26 +0200

    Edit action and view added for the users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User updated succesfully'
+        redirect_to :action => 'show'
+      else
+        flash[:notice] = 'Updating user failed'
+        redirect_to :action => 'edit'
+      end
+  end
 end
