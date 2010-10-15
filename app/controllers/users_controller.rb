b1c47b29b4e5a3bf8d549a3a282d6091fed4899f
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-10-15 21:27:01 +0200

    resolve conflicts committed in cb864d710ec16427bdbfe5b2b3203444d61fc970

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,25 +14,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-<<<<<<< HEAD
-  def new
-    @user = User.new(params[:user])
-=======
   # GET /users/new
   def new
     @user = User.new
->>>>>>> 15543acf7abfad3ed9f541e3f360c3ba53465bc7
   end
 
   # POST /users
   def create
     @user = User.new(params[:user])
-<<<<<<< HEAD
-    if @user.save
-      redirect_to root_url
-    else
-      render :action => :new
-=======
     
     if @user.save
       flash[:notice] = 'Successfully created user'
@@ -40,7 +29,6 @@ class UsersController < ApplicationController
     else
       flash[:notice] = 'Creating new user failed'
       render :action => "new"
->>>>>>> 15543acf7abfad3ed9f541e3f360c3ba53465bc7
     end
   end
 
