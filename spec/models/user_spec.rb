378d8e28ff0acff05a0370746f1fee94e2640bc1
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-10-15 22:06:37 +0200

    fix & rewrote user spec a little bit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,22 +11,31 @@ describe User do
     end
   end
 
-  context "that is already created" do
+  context "when fully created" do
     before(:each) do
-      @user = User.new     
+      @user = User.new
+      @user.username = 'edsgerd'
+      @user.password = 'abcdefg'
+      @user.password_confirmation = 'abcdefg'
+      @user.first_name = 'Edsger'
+      @user.last_name = 'Dijkstra'
+      @user.email = 'edsgerd@example.com'
+      @user.title = 'Genius'
+      @user.gender = 'm'
     end
-  end
 
-  it "should be invalid without a name" do
-    @user.should_not be_valid
-    @user.email = 'abcde@abcde.com'
-    @user.password = 'abcdefg'
-    @user.title = 'Software Developer'
-    @user.gender = 'm'
-    @user.first_name = 'Edgar'
-    @user.last_name = 'Dijkstra'
-    @user.errors.on(:username)
-    @user.usernamea = 'edijkstra'
-    @user.should be_valid
+    it "should be valid" do
+      @user.should be_valid
+    end
+
+    it "should not be valid without a username" do
+      @user.username = nil
+      @user.should_not be_valid
+    end
+
+    it "should not be valid with an invalid e-mail" do
+      @user.email = 'Muhahaha'
+      @user.should_not be_valid
+    end
   end
 end
