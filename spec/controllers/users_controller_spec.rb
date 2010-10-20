fe81677326763a99a5c9f681d2fe667f72b8c75f
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2010-10-20 21:52:46 +0200

    Add some specs to UsersController#home. Refs #9

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,4 +14,45 @@ describe UsersController do
 
     assigns[:user].should == User.find(1)
   end
+
+  context "home" do
+    context "when there are no users" do
+      it "should not display any users" do
+        User.stub(:all).and_return([])
+
+        get :home
+
+        assigns[:random_users].should == []
+      end
+    end
+
+    context "when there are users" do
+      before(:each) do
+        shuffled_list = [*1..15]
+
+        User.stub_chain( :all, :shuffle ).and_return(shuffled_list)
+      end
+
+      it "should shuffle the user list" do
+        user_list = mock('user_list')
+        user_list.should_receive(:shuffle).and_return([])
+
+        User.stub(:all).and_return(user_list)
+        get :home
+      end
+
+      # Note: This should be configurable
+      it "should return 12 users" do
+        get :home
+
+        assigns[:random_users].size.should == 12
+      end
+
+      it "should return the first 12 items of the shuffled list" do
+        get :home
+
+        assigns[:random_users].should == [*1..12]
+      end
+    end
+  end
 end
