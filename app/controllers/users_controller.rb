dcc2812261ab0a60ed47378315af6bf73bf69089
Manuel <manulorenzop@gmail.com>
2010-10-25 09:16:56 +0200

    changed partials for the user registration for the new model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,11 +28,10 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new!(params[:user])
-    @profile = @user.build_profile!(params[:profile])
-    if (@profile.save! && @user.save!)
+    @user = User.new(params[:user])
+    if @user.save
       flash[:notice] = 'Successfully created user'
-      redirect_to(@profile, :notice => 'User was successfully created.')
+      redirect_to(@user, :notice => 'User was successfully created.')
     else
       flash[:notice] = 'Creating new user failed'
       render :action => "new"
