b2ccbf1f02e76119462b327fdfb6080aacc45f93
jasperf <jasper.froeling@gmail.com>
2010-10-30 01:17:25 +0200

    fixed user_controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,57 +1,70 @@
 require 'spec_helper'
 
 describe UsersController do
-  it "should return some users" do
-    get :index
+  describe "GET index" do
+    let(:users) { [mock_model(User).as_null_object] }
+    before do
+      User.stub(:all).and_return(users)
+      get :index
+    end
 
-    assigns[:users].should == User.all.to_a
+    it "should return some users" do
+      assigns[:users].should eq(users)
+    end
+
+    it "responds with success" do
+      response.should be_success
+    end
   end
 
-  it "should return a user" do
-    user = User.new( :id => 1 )
-    User.stub(:find).with(1).and_return(user)
-    get :show, { :id => 1 }
-
-    assigns[:user].should == User.find(1)
+  describe "GET show" do
+    context "when the user exists" do
+      let(:user) { mock_model(User).as_null_object }
+      before do
+        User.stub(:find).with(1).and_return(user)
+        get :show, :id =>1
+      end
+      it "should return the user" do
+        assigns[:user].should eq user
+      end
+    end
   end
 
-  context "home" do
+  describe "GET home" do
     context "when there are no users" do
       it "should not display any users" do
         User.stub(:all).and_return([])
-
         get :home
-
-        assigns[:random_users].should == []
+        assigns[:random_users].should eq []
       end
     end
 
     context "when there are users" do
+      let(:users) {[mock_model(User).as_null_object]}
       before(:each) do
-        shuffled_list = [*1..15]
-
-        User.stub_chain( :all, :shuffle ).and_return(shuffled_list)
+        User.stub(:find).with(:all, {:include=>:profile}).and_return(users)
       end
 
       it "should shuffle the user list" do
-        user_list = mock('user_list')
-        user_list.should_receive(:shuffle).and_return([])
-
-        User.stub(:all).and_return(user_list)
+        User.stub(:shuffle).and_return(users.shuffle)
+        User.stub(:first).with(12).and_return(users)
         get :home
+        assigns[:random_users].should eq users
       end
 
       # Note: This should be configurable
       it "should return 12 users" do
+        User.stub(:shuffle).and_return(users)
+        User.stub(:first).with(12).and_return(users.first(12))
         get :home
-
-        assigns[:random_users].size.should == 12
+        assigns[:random_users].size.should eq users.first(12).size
       end
 
       it "should return the first 12 items of the shuffled list" do
+        User.stub(:shuffle).and_return(users.shuffle)
+        User.stub(:first).with(12).and_return(users.first(12))
         get :home
-
-        assigns[:random_users].should == [*1..12]
+        assigns[:random_users].should eq users.shuffle.first(12)
       end
     end
   end
