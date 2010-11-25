bb44d9534cf037a58aa7fe3dbcd5af06046f699e
Timonv <timonv@gmail.com>
2010-11-25 13:18:30 +0100

    Added specs for user/profiles controllers and made nescecary fixes.

--- app/controllers/user/profiles_controller.rb
+++ app/controllers/user/profiles_controller.rb
@@ -1,27 +1,27 @@
 class User::ProfilesController < ApplicationController
   # GET /profiles
   def index
-    @profiles = Profile.all
+    @profiles = User::Profile.all
   end
 
   # GET /profiles/1
   def show
-    @profile = Profile.find(params[:id])
+    @profile = User::Profile.find(params[:id])
   end
 
   # GET /profiles/new
   def new
-    @profile = Profile.new
+    @profile = User::Profile.new
   end
 
   # GET /profiles/1/edit
   def edit
-    @profile = Profile.find(params[:id])
+    @profile = User::Profile.find(params[:id])
   end
 
   # POST /profiles
   def create
-    @profile = Profile.new(params[:profile])
+    @profile = User::Profile.new(params[:profile])
 
     if @profile.save
       redirect_to(@profile, :notice => 'Profile was successfully created.')
@@ -32,20 +32,20 @@ class User::ProfilesController < ApplicationController
 
   # PUT /profiles/1
   def update
-    @profile = Profile.find(params[:id])
-    @user = User.find(@profile.user_id)
+    @profile = User::Profile.find(params[:id])
+    @user = @profile.user
     if @profile.update_attributes(params[:user_profile])
       flash[:notice] = 'Profile updated succesfully'
       redirect_to (@user)
     else
       flash[:notice] = 'Updating profile failed'
-      redirect_to :action => 'edit'
+      render :action => 'edit'
     end
   end
 
   # DELETE /profiles/1
   def destroy
-    @profile = Profile.find(params[:id])
+    @profile = User::Profile.find(params[:id])
     @profile.destroy
     redirect_to(profiles_url)
   end
