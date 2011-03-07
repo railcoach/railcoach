5714deb887b405123081de734db1e7cfad7ba526
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2011-03-07 20:59:11 +0100

    Add some routes to fix some specs

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,15 @@ Dynamic::Application.routes.draw do
   match 'project/membership/:membership_id/accept_invitation' => 'project/memberships#accept_invitation', :via => :put, :as => :accept_invitation_project_membership
   match 'project/membership/:membership_id/accept_member' => 'project/memberships#accept_member', :via => :put, :as => :accept_member_project_membership
 
-  #match 'user/home' => 'users#home', :as => :home_users
+  resources :users
+  match 'users/index' => 'users#index'
+  match 'users/show' => 'users#show', :as => :user
+  match 'users/home' => 'users#home', :as => :home_users
   namespace :user do
     resources :profiles
   end
 
+  resource :activations
 
   get "static/home"
   get "static/projects"
