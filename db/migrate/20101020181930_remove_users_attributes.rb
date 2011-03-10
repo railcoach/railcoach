32ea456b8e317094ffac4c364ba1bc7e4617193c
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-03-10 21:50:59 +0100

    make migration symmetric

--- db/migrate/20101020181930_remove_users_attributes.rb
+++ db/migrate/20101020181930_remove_users_attributes.rb
@@ -6,7 +6,6 @@ class RemoveUsersAttributes < ActiveRecord::Migration
   def self.down
     add_column :users, :gender, :string
     add_column :users, :title, :string
-    add_column :users, :email, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
   end
