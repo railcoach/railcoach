e2063543a463f9303616e2cf742fa52b9af10f7d
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2011-03-19 14:49:35 +0100

    Make Project::Membership a state machine (still without behavior), add attribute 'state'.

--- app/models/project/membership.rb
+++ app/models/project/membership.rb
@@ -3,6 +3,9 @@ class Project::Membership < ActiveRecord::Base
   belongs_to :user, :class_name => "User"
   has_and_belongs_to_many :roles, :class_name => "Project::Role", :join_table => "project_memberships_roles"
 
+  state_machine :initial => :none do
+  end
+
   def accept_invitation
     self.user_accepted = true
     save
