ac26a4d29899f2f15b9c94a3e99dc57807c3bd05
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-04-16 14:18:54 +0200

    Remove logic from view.

--- app/helpers/project/memberships_helper.rb
+++ app/helpers/project/memberships_helper.rb
@@ -1,2 +1,12 @@
 module Project::MembershipsHelper
+
+  def role_line(membership)
+    role_line = ''
+    membership.roles.each do |role|
+      role_line << role.name
+      role_line << ', ' unless role == membership.roles.last
+    end
+    role_line
+  end
+
 end
