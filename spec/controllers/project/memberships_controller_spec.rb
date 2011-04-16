29d76cbc8a34a3fa8db5f0c0ee201cde05c23de3
Jasper <jasper@Boson.(none)>
2011-04-16 17:39:16 +0200

    fixed all specs, but memberships in routes needs work

--- spec/controllers/project/memberships_controller_spec.rb
+++ spec/controllers/project/memberships_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Project::MembershipsController do
   let (:membership) { mock_model(Project::Membership).as_null_object }
+  let (:project) { mock_model(Project).as_null_object }
 
   before do
     Project::Membership.stub(:find).with(membership).and_return(membership)
@@ -11,10 +12,10 @@ describe Project::MembershipsController do
   describe "accept_invitation" do
     it "should call accept_invitation" do
       membership.should_receive(:accept_invitation)
-      put :accept_invitation, :membership_id => membership
+      put :accept_invitation, :membership_id => membership, :project_id => project.id
     end
     it "should redirect to back" do
-      put :accept_invitation, :membership_id => membership
+      put :accept_invitation, :membership_id => membership, :project_id => project.id
       response.should redirect_to :back
     end
   end
@@ -22,10 +23,10 @@ describe Project::MembershipsController do
   describe "accept_member" do
     it "should call accept_member" do
       membership.should_receive(:accept_member)
-      put :accept_member, :membership_id => membership
+      put :accept_member, :membership_id => membership, :project_id => project.id
     end
     it "should redirect to back" do
-      put :accept_member, :membership_id => membership
+      put :accept_member, :membership_id => membership, :project_id => project.id
       response.should redirect_to :back
     end
   end
@@ -33,10 +34,10 @@ describe Project::MembershipsController do
   describe "deny" do
     it "should destroy the membership" do
       membership.should_receive(:destroy)
-      put :deny, :membership_id => membership
+      put :deny, :membership_id => membership, :project_id => project.id
     end
     it "should redirect to back" do
-      put :deny, :membership_id => membership
+      put :deny, :membership_id => membership, :project_id => project.id
       response.should redirect_to :back
     end
   end
