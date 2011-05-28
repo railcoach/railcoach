8e58bed869a6b712ee50673b3bd098629aecb7d4
Joost Elfering <yopefonic@gmail.com>
2011-05-28 15:54:43 +0200

    resolving issue with membership roles in the old way that should now be the new way.

--- app/models/project/membership.rb
+++ app/models/project/membership.rb
@@ -1,7 +1,6 @@
 class Project::Membership < ActiveRecord::Base
   belongs_to :project, :class_name => "Project"
   belongs_to :user, :class_name => "User"
-  has_and_belongs_to_many :roles, :class_name => "Project::Role", :join_table => "project_memberships_roles"
   validates_uniqueness_of :project_id, :scope => :user_id
 
   state_machine :initial => :none do
