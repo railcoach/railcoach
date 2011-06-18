f0e07801c26b6f10ea9fdace553049be250e66df
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-06-18 15:02:45 +0200

    specs for logged in user + refactored to parameterized specs

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -10,56 +10,87 @@ describe Ability do
     context "UsersController" do
       let(:user) { User.new }
 
-      it "should allow home" do
-        ability.should be_able_to :home, User
+      [:home, :index, :show].each do |action|
+        it "should allow #{action}" do
+          ability.should be_able_to action, User
+        end
       end
 
-      it "should allow index" do
-        ability.should be_able_to :index, User
-      end
-
-      it "should allow show" do
-        ability.should be_able_to :show, user
-      end
-
-      it "should not allow edit" do
-        ability.should_not be_able_to :edit, user
-      end
-
-      it "should not allow update" do
-        ability.should_not be_able_to :update, user
-      end
-
-      it "should not allow destroy" do
-        ability.should_not be_able_to :destroy, user
+      [:edit, :update, :destroy].each do |action|
+        it "should not allow #{action}" do
+          ability.should_not be_able_to action, user
+        end
       end
     end
 
     context "ProjectsController" do
       let(:project) { Project.new }
 
-      it "should allow home" do
-        ability.should be_able_to :home, Project
+      [:home, :index, :show].each do |action|
+        it "should allow #{action}" do
+          ability.should be_able_to action, Project
+        end
       end
 
-      it "should allow index" do
-        ability.should be_able_to :index, Project
+      [:edit, :update, :destroy].each do |action|
+        it "should not allow #{action}" do
+          ability.should_not be_able_to action, project
+        end
+      end
+    end
+  end
+
+  context "when user logged in" do
+    let(:ability) { Ability.new(current_user) }
+    let(:current_user) { User.new }
+
+    before do
+      current_user.stub(:id).and_return(1)
+    end
+
+    context "UsersController" do
+      let(:other_user) { User.new }
+
+      before do
+        other_user.stub(:id).and_return(2)
       end
 
-      it "should allow show" do
-        ability.should be_able_to :show, project
+      [:edit, :update, :destroy].each do |action|
+        it "should allow #{action} on current user" do
+          ability.should be_able_to action, current_user
+        end
+
+        it "should not allow #{action} on other user" do
+          ability.should_not be_able_to action, other_user
+        end
+      end
+    end
+
+    context "ProjectsController" do
+      let(:project) { Project.new }
+
+      context "when user is an owner of the project" do
+        before do
+          current_user.should_receive(:is_owner_of?).with(project).and_return(true)
+        end
+
+        [:edit, :update, :destroy].each do |action|
+          it "should allow #{action}" do
+            ability.should be_able_to action, project
+          end
+        end
       end
 
-      it "should not allow edit" do
-        ability.should_not be_able_to :edit, project
-      end
+      context "when user is not an owner of the project" do
+        before do
+          current_user.should_receive(:is_owner_of?).with(project).and_return(false)
+        end
 
-      it "should not allow update" do
-        ability.should_not be_able_to :update, project
-      end
-
-      it "should not allow destroy" do
-        ability.should_not be_able_to :destroy, project
+        [:edit, :update, :destroy].each do |action|
+          it "should not allow #{action}" do
+            ability.should_not be_able_to action, project
+          end
+        end
       end
     end
   end
