2b31921298879e17041cb3889ecc4646bae0e766
Timon Vonk <mail@timonv.nl>
2011-06-18 17:57:09 +0200

    Updated jquery; started refactor on login popup,turend off

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
+require 'simplecov'
 require 'rubygems'
 require 'spork'
+
 
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
+  SimpleCov.start
 
 end
 
