0bbd2401873d428b9c383101696ee1cbaf42f988
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-06-19 19:17:14 +0200

    check_authorization breaks devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  check_authorization
+  #check_authorization
 
   rescue_from CanCan::AccessDenied do |exception|
     render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
