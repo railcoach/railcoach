9b511ad2f95a892c0aa0d3c33ae5df8d272ae2ae
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-06-19 16:45:47 +0200

    run simplecov in Spork.each_run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,13 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  SimpleCov.start 'rails' do
-    add_filter "/spork/"
-  end
 end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-
+  SimpleCov.start 'rails' do
+    add_filter "/spork/"
+  end
 end
 
 # --- Instructions ---
