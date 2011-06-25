7d94877a3aa3ef77389c64dc22f18947eaa1918f
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-06-25 21:50:36 +0200

    refactor users view edit and destroy links

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,5 @@
 module ProjectsHelper
-  def edit_and_destroy_links
+  def project_edit_and_destroy_links
     a = []
     a.push link_to 'Edit', edit_project_path(@project) if can? :edit, @project
     a.push link_to 'Destroy', @project, :confirm => 'Are you sure?', :method => :delete if can? :destroy, @project
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,4 +10,11 @@ module UsersHelper
     end
     return gender
   end
+
+  def user_edit_and_destroy_links
+    a = []
+    a.push link_to "Edit", edit_user_profile_path(@user.profile) if can? :edit, @user.profile
+    a.push link_to 'Destroy', @user, :confirm => 'Are you sure?', :method => :delete if can? :destroy, @user
+    a.join ' - '
+  end
 end
