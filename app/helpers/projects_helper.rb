6032aae49c110c07ace5f52b94a3a262e13c6ab9
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-06-25 20:47:10 +0200

    hide project edit/destroy when they are not allowed

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,8 @@
 module ProjectsHelper
+  def edit_and_destroy_links
+    a = []
+    a.push link_to 'Edit', edit_project_path(@project) if can? :edit, @project
+    a.push link_to 'Destroy', @project, :confirm => 'Are you sure?', :method => :delete if can? :destroy, @project
+    a.join ' - '
+  end
 end
