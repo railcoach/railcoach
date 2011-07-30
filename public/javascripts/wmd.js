ce8cc14653c7cb14fed27492ea32059cbbdf95db
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-07-30 22:00:58 +0200

    change links to projectlodge.org, hide help button (for now)

--- public/javascripts/wmd.js
+++ public/javascripts/wmd.js
@@ -37,8 +37,8 @@ Attacklab.wmdBase = function(){
 	
 	// The text that appears on the upper part of the dialog box when
 	// entering links.
-	var imageDialogText = "<p style='margin-top: 0px'><b>Enter the image URL.</b></p><p>You can also add a title, which will be displayed as a tool tip.</p><p>Example:<br />http://wmd-editor.com/images/cloud1.jpg   \"Optional title\"</p>";
-	var linkDialogText = "<p style='margin-top: 0px'><b>Enter the web address.</b></p><p>You can also add a title, which will be displayed as a tool tip.</p><p>Example:<br />http://wmd-editor.com/   \"Optional title\"</p>";
+	var imageDialogText = "<p style='margin-top: 0px'><b>Enter the image URL.</b></p><p>You can also add a title, which will be displayed as a tool tip.</p><p>Example:<br />http://projectlodge.org/images/projectlodge.png   \"Optional title\"</p>";
+	var linkDialogText = "<p style='margin-top: 0px'><b>Enter the web address.</b></p><p>You can also add a title, which will be displayed as a tool tip.</p><p>Example:<br />http://projectlodge.org/   \"Optional title\"</p>";
 	
 	// The default text that appears in the dialog input box when entering
 	// links.
@@ -53,8 +53,8 @@ Attacklab.wmdBase = function(){
 	var pastePollInterval = 100;
 	
 	// The link and title for the help button
-	var helpLink = "http://wmd-editor.com/";
-	var helpHoverTitle = "WMD website";
+	var helpLink = "#";
+	var helpHoverTitle = "Projectlodge Markdown Editing Help";
 	var helpTarget = "_blank";
 	
 	// -------------------------------------------------------------------
