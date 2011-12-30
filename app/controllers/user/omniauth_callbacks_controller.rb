2a482a7fc1c07f0a1ffc5429abe33eff3de36f2d
Timon Vonk <timonv@gmail.com>
2011-12-30 22:09:08 +0100

    Reverted last and changed to present?

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       omniauth = request.env["omniauth.auth"]
       omniauth = env["omniauth.auth"]
 
-      if current_user.persisted? #or User.find_by_email(auth.recursive_find_by_key("email"))
+      if current_user.present? #or User.find_by_email(auth.recursive_find_by_key("email"))
         current_user.user_tokens.find_or_create_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
         flash[:notice] = "Authentication successful"
         redirect_to edit_user_registration_path
@@ -16,11 +16,11 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
         if authentication
           flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth['provider']
-          sign_in_and_redirect(:user, authentication.user)
+          #sign_in_and_redirect(:user, authentication.user)
           sign_in_and_redirect(authentication.user, :event => :authentication)
         else
 
-          create a new user
+          #create a new user
           unless omniauth.recursive_find_by_key("email").blank?
             user = User.find_or_initialize_by_email(:email => omniauth.recursive_find_by_key("email"))
           else
