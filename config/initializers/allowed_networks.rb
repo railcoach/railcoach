fe45108246228fc711f83bdd0bf81df9b912096f
Timon Vonk <timonv@gmail.com>
2011-12-31 00:09:22 +0100

    fixed url typo for myopenid

--- config/initializers/allowed_networks.rb
+++ config/initializers/allowed_networks.rb
@@ -2,5 +2,5 @@ Dynamic::Application.configure do
   config.allowed_networks[:google] = {:method => :open_id, :url => "https://www.google.com/accounts/o8/id" }
   config.allowed_networks[:facebook] = {:method => :facebook}
   config.allowed_networks[:github] = { :method => :github }
-  config.allowed_networks[:openid] = { :method => :open_id, :url => "http://www.myopenid.com/affliate" }
+  config.allowed_networks[:openid] = { :method => :open_id, :url => "http://www.myopenid.com/affiliate" }
 end
